cf56eb53c8a03184fc92528181bb51e9b324db71
U-ACUMENDOMAIN\sgrover <sgrover@ACUCLE-JG9QYZ1.acumensolutions.com>
2015-06-22 15:26:53 -0400

    Added food service for open FDA food enforcement API

---
+++ services/food-service.js
@@ -0,0 +1,39 @@
+var request = require("request");
+var url = require("url");
+
+var foodService = {};
+
+foodService.options = {
+	pageSize: 10
+};
+
+foodService.apiKey = null;
+
+foodService.getFoodEnforcement = function(searchTerm, page, successCallback, errorCallback) {
+	var endpoint = new Object();
+	endpoint.protocol = "https:"
+	endpoint.host = "api.fda.gov";
+	endpoint.pathname = "/food/enforcement.json";
+	endpoint.query = {
+		"api_key": foodService.apiKey,
+		"search": "\"" + searchTerm + "\"",
+		"limit": foodService.options.pageSize,
+		"skip": page - 1 * foodService.options.pageSize
+	};
+
+	var path = url.format(endpoint);
+
+	request.get(path, function(error, response, body) {
+		if (error) {
+			errorCallback(error);
+			return;
+		}
+
+		var results = JSON.parse(body);
+		successCallback(results);
+	});
+}
+
+foodService.setApiKey = function(key) {
+	foodService.ApiKey = key;
+}
