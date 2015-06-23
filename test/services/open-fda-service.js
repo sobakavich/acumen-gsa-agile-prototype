c588840bc36212bb48adb679aa27492a5054ea01
U-ACUMENDOMAIN\sgrover <sgrover@ACUCLE-JG9QYZ1.acumensolutions.com>
2015-06-23 15:49:38 -0400

    Added empty test suite for open-fda-service module

---
+++ test/services/open-fda-service.js
@@ -0,0 +1,16 @@
+var assert = require("assert"),
+	openFdaService = require("../../services/open-fda-service");
+
+describe('open-fda-service', function(){
+
+  describe('searchFoodEnforcement', function(){
+
+    it('', function(){
+    	
+    });
+
+
+
+  });
+
+});
